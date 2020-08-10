0ddb23127ad0ff5dd95977acedda057f780314ed
GNREDDY <aryareddy1680@gmail.com>
2020-08-10 21:09:52 +0530

    updated header

--- src/App.js
+++ src/App.js
@@ -1,17 +1,19 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 import LiveScore from './component/LiveScore'
 import {BrowserRouter as Router,Route} from "react-router-dom"
+import Header from './component/Header'
+
+
 
 function App() {
-  return (
-    <div className="App">
+  return (     
       <Router>
-          
+      <Header />
           <Route path="/scorer/LiveScore" component={LiveScore} />
         </Router>
-    </div>
+        
+   
   );
 }
 
